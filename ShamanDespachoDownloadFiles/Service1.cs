0474c8bb6a4dc6f35f90bb6b07e3e20c60a02f16
jbaglione <jbaglione@paramedic.com.ar>
2020-02-11 11:16:48 -0300

    First commit

---
+++ ShamanDespachoDownloadFiles/Service1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.ServiceProcess;
+using System.Timers;
+using System.IO;
+using System.Net;
+using System.Configuration;
+using System.Data.SqlClient;
+
+
+namespace ShamanDespachoDownloadFiles
+{
+    public partial class Service1 : ServiceBase
+    {
+        Timer t = new Timer();
+        string dBServer1 = ConfigurationManager.AppSettings["DBServer1"];
+        //long interval = Convert.ToInt64(ConfigurationManager.AppSettings["Interval"]);
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            addLog(true, "OnStart", "inicio");
+            t.Elapsed += delegate { ElapsedHandler(); };
+            t.Interval = 40000;
+            t.Start();
+        }
+
+        protected override void OnPause()
+        {
+            t.Stop();
+        }
+
+        protected override void OnContinue()
+        {
+            t.Start();
+        }
+
+        protected override void OnStop()
+        {
+            t.Stop();
+        }
+
+        public void ElapsedHandler()
+        {
+            try
+            {
+                ///*------> Conecto a DB <---------*/
+                //if (this.setConexionDB())
+                //{
+                /*------> Proceso <--------*/
+                this.DownladFile();
+                //}
+            }
+            catch (Exception ex)
+            {
+                //addLog(false, "ElapsedHandler", string.Format("Exception. {0}", ex.Message));
+            }
+        }
+
+        private void addLog(bool rdo, string logProcedure, string logDescription)
+        {
+
+            string path;
+
+            path = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            path = path + "\\" + DateTime.Now.ToString("yyyy-MM-dd").Replace("-", "_") + ".log";
+
+            if (!File.Exists(path))
+            {
+                // Create a file to write to.
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    sw.WriteLine("Log " + DateTime.Now.Date);
+                }
+            }
+
+            using (StreamWriter sw = File.AppendText(path))
+            {
+                string rdoStr = "Ok";
+                if (rdo == false)
+                {
+                    rdoStr = "Error";
+                }
+                sw.WriteLine(DateTime.Now.Hour.ToString("00") + ":" + DateTime.Now.Minute.ToString("00") + "\t" + rdoStr + "\t" + logProcedure + "\t" + logDescription);
+            }
+        }
+
+        #region proceso
+
+        public void DownladFile()
+        {
+            string queryString = "SELECT inc.id, inc.Url, clf.RutaRemota + '\' + CAST(inc.IncidenteId as varchar) + '_' + cast(inc.ID as varchar) + '.' + ";
+            queryString += "SUBSTRING( RIGHT(inc.Url, 4), CHARINDEX('.', RIGHT(inc.Url, 4)) + 1, LEN(RIGHT(inc.Url, 4)) - CHARINDEX('.', RIGHT(inc.Url, 4)) ) as FTP ";
+            queryString += "FROM IncidentesAdjuntos inc INNER JOIN AdjuntosClasificaciones clf ON inc.AdjuntoClasificacionId = clf.ID ";
+            queryString += "WHERE inc.flgDescargado = 0 AND inc.Url IS NOT NULL";
+
+            //addLog(true, "DownladFile 1-queryString: ", queryString);
+            using (SqlConnection connection = new SqlConnection(dBServer1))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+                connection.Open();
+                addLog(true, "DownladFile connection.Open(): ", "OK");
+                SqlDataReader reader = command.ExecuteReader();
+                try
+                {
+                    while (reader.Read())
+                    {
+                        string incId = reader["id"].ToString();
+                        string urlOrigin = reader["Url"].ToString();
+                        string ftpSource = reader["FTP"].ToString();
+
+                        addLog(true, "DownladFile variables: ", string.Format("incId: {0},urlOrigin: {1},ftpSource: {2}, ", incId, urlOrigin, ftpSource));
+
+                        //urlOrigin = "https://www.marketingdirecto.com/wp-content/uploads/2019/10/logo-volkswagen.jpg";
+                        //ftpSource = @"\\archivos02\ftp paramedic\carpeta\image35.png";
+
+                        if (!Directory.Exists(Path.GetDirectoryName(ftpSource)))
+                        {
+                            addLog(true, "DownladFile CreateDirectory: ", Path.GetDirectoryName(ftpSource));
+                            Directory.CreateDirectory(Path.GetDirectoryName(ftpSource));
+                        }
+
+                        using (WebClient client = new WebClient())
+                        {
+                            addLog(true, "DownloadFile", "pre descarga " + @ftpSource);
+                            client.DownloadFile(new Uri(urlOrigin), @ftpSource);
+                            addLog(true, "DownloadFile", "Se descargo " + @ftpSource);
+                        }
+                        if (!updateStatus(incId, 1))
+                            if (!updateStatus(incId, 1))
+                                throw new Exception("No se puede actualizar el estado de flgDescargado.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    addLog(false, "DownloadFile", ex.Message);
+                }
+                finally
+                {
+                    // Always call Close when done reading.
+                    reader.Close();
+                }
+            }
+        }
+
+        private bool updateStatus(string incId, int flgDescargado)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(dBServer1))
+                {
+                    string queryString = "UPDATE IncidentesAdjuntos SET flgDescargado = " + flgDescargado + " WHERE id = " + incId;
+                    SqlCommand commandUpdate = new SqlCommand(queryString, connection);
+                    connection.Open();
+                    bool result = Convert.ToBoolean(commandUpdate.ExecuteNonQuery());
+                    if(result)
+                    {
+                        addLog(true, "updateStatus", "Se actualizo el estado a " + flgDescargado);
+                        return true;
+                    }
+                    else
+                    {
+                        addLog(false, "updateStatus", "No se pudo actualizar el estado a " + flgDescargado);
+                        return false;
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                addLog(false, "updateStatus", ex.Message);
+            }
+            return false;
+        }
+
+        #endregion
+    }
+
+
+}
---
+++ ShamanDespachoDownloadFilesWinForm/frmLog.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace ShamanDespachoDownloadFilesWinForm
+{
+    public partial class frmLog : Form
+    {
+        string dBServer1 = ConfigurationManager.AppSettings["DBServer1"];
+        public frmLog()
+        {
+            InitializeComponent();
+            this.tmrRefresh.Enabled = true;
+            this.tmrRefresh_Tick(null, null);
+        }
+
+        private void addLog(bool rdo, string logProcedure, string logDescription, bool clear = false)
+        {
+
+            string path;
+
+            path = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            path = path + "\\" + DateTime.Now.ToString("yyyy-MM-dd").Replace("-", "_") + ".log";
+
+            if (!File.Exists(path))
+            {
+                // Create a file to write to.
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    sw.WriteLine("Log " + DateTime.Now.Date);
+                    this.txtLog.Text = "Log " + DateTime.Now.Date + Environment.NewLine;
+                }
+            }
+
+            using (StreamWriter sw = File.AppendText(path))
+            {
+                string rdoStr = "Ok";
+                if (rdo == false)
+                {
+                    rdoStr = "Error";
+                }
+
+                sw.WriteLine(DateTime.Now.Hour.ToString("00") + ":" + DateTime.Now.Minute.ToString("00") + "\t" + rdoStr + "\t" + logProcedure + "\t" + logDescription);
+                if (clear) { this.txtLog.Text = ""; }
+                this.txtLog.Text = this.txtLog.Text + DateTime.Now.Hour.ToString("00") + ":" + DateTime.Now.Minute.ToString("00") + "\t" + rdoStr + "\t" + logProcedure + "\t" + logDescription + Environment.NewLine;
+
+            }
+
+        }
+
+        private void tmrRefresh_Tick(object sender, System.EventArgs e)
+        {
+            this.tmrRefresh.Enabled = false;
+
+            /*------> Proceso <--------*/
+            this.DownladFile();
+
+            this.tmrRefresh.Enabled = true;
+        }
+
+        #region proceso
+
+        public void DownladFile()
+        {
+            string queryString = "SELECT inc.id, inc.Url, clf.RutaRemota + '\' + CAST(inc.IncidenteId as varchar) + '_' + cast(inc.ID as varchar) + '.' + ";
+            queryString += "SUBSTRING( RIGHT(inc.Url, 4), CHARINDEX('.', RIGHT(inc.Url, 4)) + 1, LEN(RIGHT(inc.Url, 4)) - CHARINDEX('.', RIGHT(inc.Url, 4)) ) as FTP ";
+            queryString += "FROM IncidentesAdjuntos inc INNER JOIN AdjuntosClasificaciones clf ON inc.AdjuntoClasificacionId = clf.ID ";
+            queryString += "WHERE inc.flgDescargado = 0 AND inc.Url IS NOT NULL";
+
+            using (SqlConnection connection = new SqlConnection(dBServer1))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                try
+                {
+
+                    while (reader.Read())
+                    {
+                        string incId = reader["id"].ToString();
+                        string urlOrigin = reader["Url"].ToString();
+                        string ftpSource = reader["FTP"].ToString();
+
+                        //urlOrigin = "https://www.marketingdirecto.com/wp-content/uploads/2019/10/logo-volkswagen.jpg";
+                        //ftpSource = @"\\archivos02\ftp paramedic\carpeta\image35.png";
+
+                        if (!Directory.Exists(Path.GetDirectoryName(ftpSource)))
+                        {
+                            Directory.CreateDirectory(Path.GetDirectoryName(ftpSource));
+                        }
+
+                        using (WebClient client = new WebClient())
+                        {
+                            client.DownloadFile(new Uri(urlOrigin), @ftpSource);
+
+                        }
+                        if (!updateStatus(incId, 1))
+                            if (!updateStatus(incId, 1))
+                                throw new Exception("No se puede actualizar el estado de flgDescargado.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    addLog(false, "DownloadFile", ex.Message);
+                }
+                finally
+                {
+                    // Always call Close when done reading.
+                    reader.Close();
+                }
+            }
+        }
+
+        private bool updateStatus(string incId, int flgDescargado)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(dBServer1))
+                {
+                    string queryString = "UPDATE IncidentesAdjuntos SET flgDescargado = " + flgDescargado + " WHERE id = " + incId;
+                    SqlCommand commandUpdate = new SqlCommand(queryString, connection);
+                    connection.Open();
+                    return Convert.ToBoolean(commandUpdate.ExecuteNonQuery());
+                }
+            }
+            catch (Exception ex)
+            {
+                addLog(false, "updateStatus", ex.Message);
+            }
+            return false;
+        }
+
+        #endregion
+    }
+}
